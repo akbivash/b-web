a7ace36d3451ea5ad61851e977eff8d7440b94df
akbiv <akbivash@gmail.com>
2024-01-07 12:49:07 +0545

    Add new project

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,31 +4,31 @@ import {SiPostgresql} from 'react-icons/si'
 // import image from '../assets/jobhouse.png'
 
 export const projects = [
-    {
-        title:'TravelMate',
-        description:'A hotel booking app',
-        image:'https://w0.peakpx.com/wallpaper/683/262/HD-wallpaper-hotel-booking-online-reservation-mobile-application-for-renting-3050891-vector-art-at-vecteezy.jpg',
-        url:'https://travelmate-96.netlify.app/',
-        github:'https://github.com/akbivash/travel_mate-booking-app',
-        stacks:[
-            {title:'React', icon:FaReact},
-            {title:'Node', icon:FaNode},
-            {title:'MongoDB', icon:BiLogoMongodb},
-            {title:'TypeScript', icon:BiLogoTypescript}
-        ]
-    },
-    {
-        title:'FashioNepa',
-        description:'An e-commerce app',
-        image:'https://img.freepik.com/free-photo/dark-haired-woman-with-red-lipstick-smiles-leans-stand-with-clothes-holds-package-pink-background_197531-17609.jpg?w=740&t=st=1674103608~exp=1674104208~hmac=893cb53e9ca643c3ac1faf3d4f48427174214f9729e4e90daff66f5315bd747e',
-        url:'https://fashionepa.netlify.app/',
-        github:'https://github.com/akbivash/fashio_nepa-frontend',
-        stacks:[
-            {title:'React', icon:FaReact},
-            {title:'Node', icon:FaNode},
-            {title:'MongoDB', icon:BiLogoMongodb}
-        ]
-    },
+    // {
+    //     title:'TravelMate',
+    //     description:'A hotel booking app',
+    //     image:'https://w0.peakpx.com/wallpaper/683/262/HD-wallpaper-hotel-booking-online-reservation-mobile-application-for-renting-3050891-vector-art-at-vecteezy.jpg',
+    //     url:'https://travelmate-96.netlify.app/',
+    //     github:'https://github.com/akbivash/travel_mate-booking-app',
+    //     stacks:[
+    //         {title:'React', icon:FaReact},
+    //         {title:'Node', icon:FaNode},
+    //         {title:'MongoDB', icon:BiLogoMongodb},
+    //         {title:'TypeScript', icon:BiLogoTypescript}
+    //     ]
+    // },
+    // {
+    //     title:'FashioNepa',
+    //     description:'An e-commerce app',
+    //     image:'https://img.freepik.com/free-photo/dark-haired-woman-with-red-lipstick-smiles-leans-stand-with-clothes-holds-package-pink-background_197531-17609.jpg?w=740&t=st=1674103608~exp=1674104208~hmac=893cb53e9ca643c3ac1faf3d4f48427174214f9729e4e90daff66f5315bd747e',
+    //     url:'https://fashionepa.netlify.app/',
+    //     github:'https://github.com/akbivash/fashio_nepa-frontend',
+    //     stacks:[
+    //         {title:'React', icon:FaReact},
+    //         {title:'Node', icon:FaNode},
+    //         {title:'MongoDB', icon:BiLogoMongodb}
+    //     ]
+    // },
     {
         title:'B-Tube',
         description:'YouTube clone',
@@ -52,6 +52,16 @@ export const projects = [
             {title:'Node', icon:FaNode},
             {title:'TypeScript', icon:BiLogoTypescript},
             {title:'Postgresql', icon:SiPostgresql}
+        ]
+    },
+    {
+        title:'Bivcryp',
+        description:'Landing page',
+        image:require('../assets/bivcryp.png'),
+        url:'https://bivcryp.netlify.app/',
+        github:'https://github.com/akbivash/bivcryp',
+        stacks:[
+            {title:'React', icon:FaReact},
         ]
     }
 ]
